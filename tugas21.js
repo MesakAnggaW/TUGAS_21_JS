c0f2771ef5ed65b7c7dc41def3616e516e5d6cb6
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-18 16:51:28 +0700

    Mengerjakan Tugas 21 javascript

---
+++ tugas21.js
@@ -0,0 +1,15 @@
+// let nama = "Mesak Angga Wibisono";
+// let nama2 = ["Nurul", "Yunia", "Sari"];
+// console.log(nama.split(" "));
+// console.log(nama.replace("Mesak", "Muhammad"));
+
+// nama = nama.split(" ");
+// console.log(nama.reverse());
+// console.log(nama.concat(nama2));
+
+// console.log(nama.slice(0, 2));
+
+let kalimat = "Saya belajar masak nasi goreng";
+
+console.log(kalimat);
+console.log(kalimat.replace("masak", "makan"));
